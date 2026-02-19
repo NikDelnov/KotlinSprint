fa922e786fe69ebc97c4ea88d341f0efc09ead84
NikDelnov <Delen88@mail.ru>
2026-02-19 21:44:29 +0300

    KS-2-1 lesson2_task1 add

--- app/src/main/kotlin/lesson2/lesson2_task1.kt
+++ app/src/main/kotlin/lesson2/lesson2_task1.kt
@@ -6,7 +6,11 @@ fun main () {
     val score3 = 3
     val score4 = 5
 
-    val averageScore = (score1 + score2 + score3 + score4) / 4.0
+    val studentCount = 4
+
+    val sum = score1 + score2 + score3 + score4
+
+    val averageScore = sum / studentCount.toDouble()
 
     println("%.2f".format(averageScore))
 }
