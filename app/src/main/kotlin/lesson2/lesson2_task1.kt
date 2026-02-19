cfba910880a716d9f8bb79deb561293174f72315
NikDelnov <Delen88@mail.ru>
2026-02-19 19:38:40 +0300

    KS-2-1 lesson2_task1

---
+++ app/src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package lesson2
+
+fun main () {
+    val score1 = 3
+    val score2 = 4
+    val score3 = 3
+    val score4 = 5
+
+    val averageScore = (score1 + score2 + score3 + score4) / 4.0
+
+    println("%.2f".format(averageScore))
+}
