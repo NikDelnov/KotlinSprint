aa6b370b3e901a2825e196fec347a01039465a4b
NikDelnov <Delen88@mail.ru>
2026-02-23 23:20:23 +0200

    update main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Apply the shared build logic from a convention plugin.
     // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
-    id("buildsrc.convention.kotlin-jvm")
+    kotlin("jvm")  // вместо buildsrc.convention.kotlin-jvm
 
     // Apply the Application plugin to add support for building an executable JVM application.
     application
